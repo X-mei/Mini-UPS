b4b3953131cd4b5588332722e6953c37d397452a
VCM admin account <vcm@vcm-19617.vm.duke.edu>
2021-04-22 11:27:24 -0400

    docker does not work

--- docker-deploy/mysite/src/database.py
+++ docker-deploy/mysite/src/database.py
@@ -7,7 +7,7 @@ import smtplib
 #connect with the database
 def db_connect():
     try:
-        conn = psycopg2.connect(database='postgres', user='postgres', password="passw0rd", host='127.0.0.1', port='5432')
+        conn = psycopg2.connect(database='postgres', user='postgres', password="passw0rd", host='db', port='5432')
         print("Connected to database successfully.")
         return conn
     except:
